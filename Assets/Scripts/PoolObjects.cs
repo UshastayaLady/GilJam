8ed8affdcd89b18b8869c99735286c586e5d6bf2
B3441 <1arya11stark@gmail.com>
2026-02-21 13:52:34 +0300

    создание рандомного спама грядок

--- Assets/Scripts/PoolObjects.cs
+++ Assets/Scripts/PoolObjects.cs
@@ -2,23 +2,24 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ListObjects : MonoBehaviour
+public class PoolObjects : MonoBehaviour
 {
-    [SerializeField] private Sprite obgectForPool;    
+    [SerializeField] private SpriteRenderer obgectForPool;    
     [SerializeField] private int countObgects;
 
-    private List<Sprite> poolObgect;
+    private List<SpriteRenderer> poolObgect;
     private readonly Vector2 pointSpawn = new Vector2(0, 0);
 
     private void Awake()
     {
+        poolObgect = new List<SpriteRenderer>();
         InitializationPoolObjects();
         ClosePoolObjects();
     }
 
     private void InitializationPoolObjects()
     {
-        for (int i=0; i < countObgects; i++)
+        for (int i = 0; i < countObgects; i++)
         {
             poolObgect.Add(Instantiate(obgectForPool, pointSpawn, Quaternion.identity));
         }
@@ -34,17 +35,17 @@ public class ListObjects : MonoBehaviour
     private void AddPoolObjects()
     {
         poolObgect.Add(Instantiate(obgectForPool, pointSpawn, Quaternion.identity));
-        poolObgect[poolObgect.Count-1].GameObject().SetActive(false);
+        poolObgect[poolObgect.Count - 1].GameObject().SetActive(false);
     }
 
-    public Sprite GetObjectInPool()
+    public SpriteRenderer GetObjectInPool()
     {
         for (int i = 0; i < poolObgect.Count; i++)
         {
-            if (poolObgect[i].GameObject().activeSelf)
+            if (!poolObgect[i].GameObject().activeSelf)
             {
                 return poolObgect[i];
-            }  
+            }
         }
         AddPoolObjects();
         return poolObgect[poolObgect.Count - 1];
